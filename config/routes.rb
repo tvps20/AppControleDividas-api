906fab6d3dc7e7711a1e55032a4e2b8b73bca6df
Thiago <thiago.vps20@gmail.com>
2018-03-17 13:15:23 +0000

    Config the routes file for the api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Subdomain troca o www por api, path troca o /api por outra nome do namespace ex: api.sites.com/controle...
+  namespace :api, defaults: { format: :json }, constraints: {subdomain: 'api'}, path: "/" do
+
+  end
+
 end
